c733ebce361b9273832d8352619601ff5d623ec2
maulana <maulana@katadata.co.id>
2022-07-12 10:28:40 +0700

    fix pwa

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
     <script>
-            var BASE_URL = '<?= base_url() ?>';
+            var BASE_URL = '/';
             document.addEventListener('DOMContentLoaded', init, false);
 
             function init() {
